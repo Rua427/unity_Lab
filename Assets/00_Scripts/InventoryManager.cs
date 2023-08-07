e6c194021f89c115065a029c02e228c608dfb5b3
Rua427 <ekrua427@naver.com>
2023-08-07 22:58:18 +0900

    inventory

---
+++ Assets/00_Scripts/InventoryManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class InventoryManager : MonoBehaviour
+{
+    public static InventoryManager Instance;
+    public List<Item> items = new();
+
+    public Transform ItemContent;
+    public GameObject InventoryItem;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void Add(Item item)
+    {
+        items.Add(item);
+    }
+
+    public void Remove(Item item)
+    {
+        items.Remove(item);
+    }
+
+
+    public void ListItems()
+    {
+        foreach (Transform item in ItemContent)
+        {
+            Destroy(item.gameObject);
+        }
+        foreach (var item in items)
+        {
+            GameObject obj = Instantiate(InventoryItem, ItemContent);
+            TextMeshProUGUI itemName = obj.transform.Find("ItemName").GetComponent<TextMeshProUGUI>();
+            var itemIcon = obj.transform.Find("ItemIcon").GetComponent<Image>();
+
+            itemName.text = item.itemName;
+            itemIcon.sprite = item.icon;
+
+        }
+    }
+}
