6394a29f14b7d7ec5311976ce933dc772ce54985
Rua427 <ekrua427@naver.com>
2023-08-08 23:04:29 +0900

    inventory

--- Assets/00_Scripts/InventoryManager.cs
+++ Assets/00_Scripts/InventoryManager.cs
@@ -12,6 +12,10 @@ public class InventoryManager : MonoBehaviour
     public Transform ItemContent;
     public GameObject InventoryItem;
 
+    public Toggle EnableRemove;
+
+    public InventoryItemController[] inventoryItems;
+
     private void Awake()
     {
         Instance = this;
@@ -39,10 +43,38 @@ public class InventoryManager : MonoBehaviour
             GameObject obj = Instantiate(InventoryItem, ItemContent);
             TextMeshProUGUI itemName = obj.transform.Find("ItemName").GetComponent<TextMeshProUGUI>();
             var itemIcon = obj.transform.Find("ItemIcon").GetComponent<Image>();
+            Button removeButton = obj.transform.Find("RemoveButton").GetComponent<Button>();
 
             itemName.text = item.itemName;
             itemIcon.sprite = item.icon;
 
+            if (EnableRemove.isOn)
+            {
+                removeButton.gameObject.SetActive(true);
+            }
+
+        }
+        SetInventoryItems();
+    }
+
+    public void EnableItemsRemove()
+    {
+        if (ItemContent == null) { return; }
+
+        // 해당 transform의 자식 오브젝트를 가져온다
+        foreach (Transform item in ItemContent)
+        {
+            item.Find("RemoveButton").gameObject.SetActive(EnableRemove.isOn);
+        }
+    }
+
+    public void SetInventoryItems()
+    {
+        inventoryItems = ItemContent.GetComponentsInChildren<InventoryItemController>();
+
+        for (int i = 0; i < items.Count; i++)
+        {
+            inventoryItems[i].AddItem(items[i]);
         }
     }
 }
